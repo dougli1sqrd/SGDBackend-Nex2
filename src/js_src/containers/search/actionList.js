bd968fd599a78cd09c82185a1e0da37b17e89da5
Shuai Weng <sweng@stanford.edu>
2020-09-11 10:32:54 -0700

    testing

--- src/js_src/containers/search/actionList.js
+++ src/js_src/containers/search/actionList.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 class ActionList extends Component {
   render() {
-    let action_categories = ['locus', 'reference', 'reserved_name', 'download'];
+    let action_categories = ['locus', 'reference', 'reserved_name', 'download', 'Allele'];
     if(action_categories.includes(this.props.category)){
       if(this.props.category == 'download'){
         let dname = this.props.display_name ? this.props.display_name : '';
@@ -17,7 +17,7 @@ class ActionList extends Component {
       }
       else{
         let href = `curate${this.props.href}`;	  
-        return <Link style={{ display: 'inline-block', minWidth: '6rem' }} to={href}><i className='fa fa-edit' /> Curate HELLO</Link>;
+        return <Link style={{ display: 'inline-block', minWidth: '6rem' }} to={href}><i className='fa fa-edit' /> Curate</Link>;
       }
     }
     return null;
