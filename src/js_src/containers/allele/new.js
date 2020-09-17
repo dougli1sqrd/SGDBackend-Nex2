1c90fa792ef45669471e1faa1594a70d84483200
Shuai Weng <sweng@stanford.edu>
2020-09-16 21:30:07 -0700

    testing

--- src/js_src/containers/allele/new.js
+++ src/js_src/containers/allele/new.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CurateLayout from '../curateHome/layout';
-// import NewALLELE from './newAllele';
+import NewALLELE from './newAllele';
 
 class NewAllele extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class NewAllele extends Component {
     return (
       <CurateLayout>
         <h1>Add Allele</h1>
-        NewALLELE
+        <NewALLELE />
       </CurateLayout>
     );
   }
