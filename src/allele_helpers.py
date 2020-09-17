2cf3fd04bebae86e29b4dad62a6706c08c92179c
Shuai Weng <sweng@stanford.edu>
2020-09-17 08:18:13 -0700

    testing

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -61,29 +61,38 @@ def insert_allele(curator_session, CREATED_BY, source_id, allele_name, so_id):
         return returnValue
 
 
-def get_so_children(parent_id, so_id_list):
+def get_so_children(parent_id, parent_id_to_child_ids, so_id_to_so, data, so_id_list):
 
     if parent_id not in so_id_list:
         so_id_list.append(parent_id)
-        
-    so_ids = DBSession.query(SoRelation.child_id).distinct(SoRelation.child_id).filter_by(parent_id=parent_id).all()    
-    for so_id in so_ids:
-        get_so_children(so_id, so_id_list)
+        s = so_id_to_so[parent_id]
+        data.append( { 'so_id': s.so_id,
+                       'format_name': s.term_name,
+                       'display_name': s.display_name } ]
+    child_ids = parent_id_to_child_ids[parent_id]
+    for child_id in child_ids:
+        get_so_children(child_id, parent_id_to_child_ids, so_id_to_so, data, so_id_list)
 
 def get_all_allele_types(request):
 
     try:
+        
+        so_id_to_so = dict([(x.so_id, x) for x in nex_session.query(So).all()])
+        
+        parent_id_to_child_ids = {}
+        for x in nex_session.query(SoRelation).all():
+            child_ids = []
+            if x.parent_id in parent_id_to_child_ids:
+                child_ids = parent_id_to_child_ids[x.parent_id]
+            child_ids.append(x.child_id)
+
+        so_id_list = []
+        data = []
+
         s = DBSession.query(So).filter_by(display_name=PARENT_SO_TERM).one_or_none()
-        data = [ { 'so_id': s.so_id,
-                   'format_name': s.term_name,
-                   'display_name': s.display_name } ]
-        so_id_list = []    
-        get_so_children(s.so_id, so_id_list)
-        for x in DBSession.query(So).filter_by(so_id=so_id).all():
-            if x.so_id in so_id_list:
-                data.append( { 'so_id': x.so_id,
-                               'format_name': x.term_name,
-                               'display_name': x.display_name } )        
+        root_parent_id = s.so_id
+        get_so_children(root_parent_id, parent_id_to_child_ids, so_id_to_so, data, so_id_list)
+        
         return HTTPOk(body=json.dumps(data),content_type='text/json')
     except Exception as e:
         log.error(e)
