f44910c3fb43dead9b0c64a47cf0f22e74d0f21b
Shuai Weng <sweng@stanford.edu>
2020-09-16 21:41:21 -0700

    testing

--- src/js_src/containers/allele/newAllele.js
+++ src/js_src/containers/allele/newAllele.js
@@ -12,84 +12,13 @@ class NewAllele extends Component {
   constructor(props) {
     super(props);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleResetForm = this.handleResetForm.bind(this);
-    
-    // this.state = {};
-  }
-
-  handleChange() {
-    let currentAllele = {};
-    let data = new FormData(this.refs.form);
-    for (let key of data.entries()) {
-      currentAllele[key[0]] = key[1];
-    }
-    this.props.dispatch(setAllele(currentAllele));
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    let formData = new FormData();
-    for(let key in this.props.allele){
-      formData.append(key,this.props.allele[key]);
-    }
-
-    fetchData(ADD_ALLELE, {
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false
-    }).then((data) => {
-      this.props.dispatch(setMessage(data.success));
-    }).catch((err) => {
-      this.props.dispatch(setError(err.error));
-    });
-  }
-
-  handleResetForm() {
-    let currentAllele = {
-      id: 0,
-      allele_name: '',
-      allele_name_pmids: '',
-      affected_gene: '',
-      affected_gene_pmids: '',	
-      so_id: '',
-      desc: '',
-      primary_pmids: '',
-      additional_pmids: '',
-      review_pmids: ''
-    };
-    this.props.dispatch(setAllele(currentAllele));
-  }
-
-  addButton() {
-    return (
-      <div>
-        <div className='row'>
-          <div className='columns medium-6'>
-            <button type='submit' className="button expanded" >Add Allele</button>
-          </div>
-        </div>
-      </div>
-    );
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleResetForm = this.handleResetForm.bind(this);
   }
 
   render() {
-
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} ref='form'>
-          <input name='id' value={this.props.allele.id} className="hide" />
-
-          // <OneAllele allele={this.props.allele} onOptionChange={this.handleChange} />
-	  Call OneAllele
-          {this.addButton()}
-
-        </form>
-
-      </div>
-    );
+    return (<div> NEW ALLELE FORM </div>);
   }
 }
 
