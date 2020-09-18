17bbf4465374a34cb4b5a385e5b909c171622516
Shuai Weng <sweng@stanford.edu>
2020-09-18 11:33:23 -0700

    testing

--- src/js_src/containers/allele/oneAllele.js
+++ src/js_src/containers/allele/oneAllele.js
@@ -32,8 +32,10 @@ class OneAllele extends Component {
 
 	
 
-        {/* Allele_type */}
-        <AutocompleteSection sec_title='Allele type' id='allele_id' value1='display_name' value2='format_name' placeholder='Enter allele type' selectedIdName='allele_id' onOptionChange={this.props.onOptionChange} selectedId={this.allele.so_id} setNewValue={false} />
+
+        {/* Allele */}
+        <AutocompleteSection sec_title='Allele' id='allele_id' value1='display_name' value2='' selectedIdName='allele_id' placeholder='Enter allele' onOptionChange={this.props.onOptionChange} selectedId={this.props.phenotype.allele_id} setNewValue={true} />
+
 
         {/* desctription */}
         <CommentSection sec_title='Description' name='desc' value={this.props.allele.desc} onOptionChange={this.props.onOptionChange} placeholder='Enter description' rows='3' cols='500' />
