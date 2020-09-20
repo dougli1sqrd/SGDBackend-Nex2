e7bee110c76cd894f8df4ce89cf621ec03be5236
Shuai Weng <sweng@stanford.edu>
2020-09-19 23:06:09 -0700

    testing

--- src/js_src/containers/allele/oneAllele.js
+++ src/js_src/containers/allele/oneAllele.js
@@ -37,7 +37,8 @@ class OneAllele extends Component {
         {/* Allele type & references */}
         <div className='row'>
           <div className='columns medium-6 small-6'>
-            <AutocompleteSection sec_title='Allele type' id='so_id' value1='display_name' value2='' selectedIdName='so_id' placeholder='Enter allele type' onOptionChange={this.props.onOptionChange} selectedId={this.props.allele.so_id} setNewValue={false} /> 
+            <div> <label> Allele type </label> </div>
+            <AutocompleteSection sec_title='' id='so_id' value1='display_name' value2='' selectedIdName='so_id' placeholder='Enter allele type' onOptionChange={this.props.onOptionChange} selectedId={this.props.allele.so_id} setNewValue={false} /> 
           </div>
           <div className='columns medium-6 small-6'>
             <div> <label> Allele type PMIDs (optional) </label> </div>
