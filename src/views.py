1d922b9ed3a8101ec6af87b5502693d13b22cdcd
Shuai Weng <sweng@stanford.edu>
2020-10-02 13:28:59 -0700

    adding allele names/aliases to the search

--- src/views.py
+++ src/views.py
@@ -193,13 +193,14 @@ def search(request):
     ]
 
 
+
     
     ## added for allele search
-    if is_quick_flag == 'true' and terms_digits_flag == False and ( '-' in query or 'delta' in query or 'Δ' in query):
-        allele_name = query.strip().replace('delta', 'Δ').upper()
-        maybe_allele_url = DBSession.query(Dbentity.obj_url).filter_by(display_name.upper() = allele_name).one_or_none()
+    if is_quick_flag == 'true' and terms_digits_flag == False and ( '-' in query or 'delta' in query):
+        allele_name = query.strip().lower()
+        maybe_allele_url = DBSession.query(Dbentity.obj_url).filter_by(display_name=allele_name).one_or_none()
         if maybe_allele_url is None:
-            aa = DBSession.query(AlleleAlias).filter_by(display_name.upper() = allele_name).one_or_none()
+            aa = DBSession.query(AlleleAlias).filter_by(display_name=allele_name).one_or_none()
             if aa is not None:
                 maybe_allele_url = aa.allele.obj_url
         if maybe_allele_url:
