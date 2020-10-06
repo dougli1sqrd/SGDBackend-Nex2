cd0536e22e7dd3b3e99ee21436556fe251b6ce5a
Shuai Weng <sweng@stanford.edu>
2020-10-06 15:41:30 -0700

    testing

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -331,6 +331,8 @@ def add_allele_data(request):
             return HTTPBadRequest(body=json.dumps({'error': "Allele type field is blank"}), content_type='text/json')
 
         desc = request.params.get('desc')
+
+        return HTTPBadRequest(body=json.dumps({'error': "ALLELE_NAME"}), content_type='text/json')
         
         returnValue = insert_allele(curator_session, CREATED_BY, source_id, allele_name, so_id, desc)
         allele_id = None
