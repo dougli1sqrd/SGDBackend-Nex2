fa32eb36bcb924292a64b2a25c6541be9efe1c43
Shuai Weng <sweng@stanford.edu>
2020-10-06 20:32:51 -0700

    testing

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -349,6 +349,12 @@ def add_allele_data(request):
         
         allele_name_pmids = request.params.get('allele_name_pmids')
 
+
+        
+        return HTTPBadRequest(body=json.dumps({'error': "allele_name_pmids="+str(allele_name_pmids)}), content_type='text/json')
+
+    
+        
         pmid_to_reference_id = dict([(x.pmid, x.dbentity_id) for x in DBSession.query(Referencedbentity).all()])
 
         (reference_ids, err_message) = check_pmids(allele_name_pmids, pmid_to_reference_id)
