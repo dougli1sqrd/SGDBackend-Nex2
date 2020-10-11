a583f25a5bafb1bdd4856ee35ed7affcb20387f6
Shuai Weng <sweng@stanford.edu>
2020-10-11 12:28:32 -0700

    testing

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -350,7 +350,7 @@ def add_allele_data(request):
             return HTTPBadRequest(body=json.dumps({'error': returnValue}), content_type='text/json')
 
         
-        # return HTTPBadRequest(body=json.dumps({'error': "allele_id="+str(allele_id)}), content_type='text/json')
+        return HTTPBadRequest(body=json.dumps({'error': "allele_id="+str(allele_id)}), content_type='text/json')
     
         
         allele_name_pmids = request.params.get('allele_name_pmids')
