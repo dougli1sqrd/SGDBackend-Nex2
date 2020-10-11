b7d4f51b067da9e1ec637b98caacc597c0af2181
Shuai Weng <sweng@stanford.edu>
2020-10-11 13:07:31 -0700

    testing

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -403,13 +403,8 @@ def add_allele_data(request):
             
             affected_gene_pmids = request.params.get('affected_gene_pmids')
 
-
-            
-            return HTTPBadRequest(body=json.dumps({'error': "affected_gene_pmids="+affected_gene_pmids}), content_type='text/json')
-
-
-        
-        
+            # return HTTPBadRequest(body=json.dumps({'error': "affected_gene_pmids="+affected_gene_pmids}), content_type='text/json')
+                
             (reference_ids, err_message) = check_pmids(affected_gene_pmids, pmid_to_reference_id)
 
             
