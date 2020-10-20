0a3735d80c308f7a24ca2393db4c9f16c3a721dd
Shuai Weng <sweng@stanford.edu>
2020-10-20 12:37:57 -0700

    testing

--- src/views.py
+++ src/views.py
@@ -194,7 +194,8 @@ def search(request):
     ]
 
     ## added for allele search
-    if is_quick_flag == 'true' and terms_digits_flag == False and ( '-' in query or 'delta' in query or 'Δ' in query):
+    query = query.replace('Δ', "delta")
+    if is_quick_flag == 'true' and terms_digits_flag == False and ( '-' in query or 'delta' in query):
         allele_name = query.strip().lower()
         maybe_allele_url = DBSession.query(Dbentity.obj_url).filter_by(subclass='ALLELE').filter_by(display_name=allele_name).one_or_none()
         if maybe_allele_url is None:
