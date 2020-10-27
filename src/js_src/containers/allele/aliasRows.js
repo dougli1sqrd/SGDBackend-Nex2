aabf4bcdfd7c94faf0ad5cf33e648837e9beba4f
Shuai Weng <sweng@stanford.edu>
2020-10-27 15:56:08 -0700

    testing

--- src/js_src/containers/allele/aliasRows.js
+++ src/js_src/containers/allele/aliasRows.js
@@ -4,25 +4,26 @@ import PropTypes from 'prop-types';
 class AliasRows extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // just to have this section in case we need to do something here
-      sec_title: props.sec_title,
-      sec_title2: props.sec_title2
-    };
   }
-
-
     
-  render() {
+  render() {	
     return (
       <div className='row'>
         <div className='columns medium-6 small-6'>
           <div> <label> Alias name </label> </div>
-          <input type='text' name='alias_name' value={this.props.value} onChange={this.props.onOptionChange} />
+
+          { for (let i = 0; i < this.props.count; i++) {
+            return <input type='text' name='alias_name{i}' value={this.props.value} onChange={this.props.onOptionChange} />
+          }}
+	    
         </div>
         <div className='columns medium-6 small-6'>
           <div> <label> PMID(s) for alias name (optional) </label> </div>
-          <input type='text' name='alias_pmids' value={this.props.value2} onChange={this.props.onOptionChange} />
+
+          { for (let i = 0; i < this.props.count; i++) {
+            return <input type='text' name='alias_pmids' value={this.props.value2} onChange={this.props.onOptionChange} />
+          }}
+	
         </div>
       </div>
     );
@@ -30,7 +31,7 @@ class AliasRows extends Component {
 }
 
 AliasRows.propTypes = {
-  count: PropTypes.interger,    
+  count: PropTypres.interger,    
   value: PropTypes.string,
   onOptionChange: PropTypes.func,
   sec_title2: PropTypes.string,
