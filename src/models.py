d8541f762b08fb478008bd5bc70c8a8ccceb224d
Shuai Weng <sweng@stanford.edu>
2020-11-12 12:59:52 -0800

    testing

--- src/models.py
+++ src/models.py
@@ -9820,7 +9820,7 @@ class Alleledbentity(Dbentity):
         #            references.append(x.reference.to_dict_citation())
         #            found[x.reference.dbentity_id] = 1
                     
-        literatureannotation
+        # literatureannotation
         all_la = DBSession.query(Literatureannotation).filter_by(dbentity_id=self.dbentity_id).all() 
         for x in all_la:
             if x.reference.dbentity_id not in found:
