4530576ec7ed75e57a3e767203cc69c62fcef4e5
Shuai Weng <sweng@stanford.edu>
2021-01-14 14:52:11 -0800

    testing

--- src/helpers.py
+++ src/helpers.py
@@ -52,8 +52,13 @@ else:
 # safe return returns None if not found instead of 404 exception
 def extract_id_request(request, prefix, param_name='id', safe_return=False):
     id = str(request.matchdict[param_name])
-
-    db_id = disambiguation_table.get(("/" + prefix + "/" + id).upper())
+    db_id = None
+    if prefix == 'reference' and id.startswith('S00'):
+        ref = DBSession.query(Referencedbentity).filter(or_(Referencedbentity.sgdid==id, Referencedbentity.pmid==str(id))).one_or_none()
+        if ref:
+            return ref.dbentity_id
+    else:
+        db_id = disambiguation_table.get(("/" + prefix + "/" + id).upper())
 
     if db_id is None and safe_return:
         return None
