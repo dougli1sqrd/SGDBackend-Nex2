d9ed6d4803b8c3155322b0f4f9e76f46a1de6336
Shuai Weng <sweng@stanford.edu>
2021-01-14 14:35:11 -0800

    testing

--- src/views.py
+++ src/views.py
@@ -712,10 +712,10 @@ def reference_go_details(request):
 @view_config(route_name='reference_phenotype_details', renderer='json', request_method='GET')
 def reference_phenotype_details(request):
     try:
-
-        return "HELLO THERE"
-        
-        id = extract_id_request(request, 'reference')
+        # id = extract_id_request(request, 'reference')
+        id = str(request.matchdict['id']
+        return "Hi " + id
+                 
         reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
 
         if reference:
