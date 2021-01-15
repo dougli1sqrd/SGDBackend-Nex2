cff66191767b9e3831021007c7f90587eb072b45
Shuai Weng <sweng@stanford.edu>
2021-01-14 20:37:43 -0800

    testing

--- src/views.py
+++ src/views.py
@@ -713,11 +713,13 @@ def reference_go_details(request):
 def reference_phenotype_details(request):
     try:
         id = extract_id_request(request, 'reference')
-        reference = None
-        if id.startswith('S00'):
-            reference = DBSession.query(Dbentity).filter_by(subclass='REFERENCE', sgdid=id).one_or_none()
-        else:
-            reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
+        return id
+        # reference = None
+        # if id.startswith('S00'):
+        #    reference = DBSession.query(Dbentity).filter_by(subclass='REFERENCE', sgdid=id).one_or_none()
+        # else:
+        #    reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
+        reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
 
         if reference:
             return reference.phenotype_to_dict()
