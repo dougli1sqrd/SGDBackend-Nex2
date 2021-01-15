346c4e5f108c56b4f91137fb242d2142e0dcfb90
Shuai Weng <sweng@stanford.edu>
2021-01-14 20:44:38 -0800

    testing

--- src/views.py
+++ src/views.py
@@ -713,11 +713,11 @@ def reference_go_details(request):
 def reference_phenotype_details(request):
     try:
         id = extract_id_request(request, 'reference')
-        return id
-    
-        # reference = None
-        # if id.startswith('S00'):
-        #    reference = DBSession.query(Dbentity).filter_by(subclass='REFERENCE', sgdid=id).one_or_none()
+        reference = None
+        if id.startswith('S00'):
+            reference = DBSession.query(Dbentity).filter_by(subclass='REFERENCE', sgdid=id).one_or_none()
+            return reference.display_name
+        
         # else:
         #    reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
         reference = DBSession.query(Referencedbentity).filter_by(dbentity_id=id).one_or_none()
