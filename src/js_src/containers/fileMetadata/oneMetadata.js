7dabbee298650985b2941b691237008bba736713
Shuai Weng <sweng@stanford.edu>
2021-01-17 15:53:00 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TwoColTextField from '../allele/twoColTextField';
+import FourColTextField from './fourColTextField';
 // import AutocompleteSection from '../phenotype/autocompleteSection';
 
 class OneMetadata extends Component {
@@ -24,8 +25,8 @@ class OneMetadata extends Component {
         {/* file display name & previous file name*/}
         <TwoColTextField sec_title='File display name' name='display_name' value={this.props.metadata.display_name} onOptionChange={this.props.onOptionChange} sec_title2='Previous file name' name2='previous_file_name' value2={this.props.metadata.previous_file_name} onOptionChange2={this.props.onOptionChange} />
 
-        {/* year & file size */}
-        <TwoColTextField sec_title='Year' name='year' value={this.props.metadata.year} onOpctionChange={this.props.onOptionChange} sec_title2='File size' name2='file_size' value2={this.props.metadata.file_size} onOptionChange2={this.props.onOptionChange} />
+        {/* file year, date, size & extension */}
+        <FourColTextField sec_title='Year' name='year' value={this.props.metadata.year} onOpctionChange={this.props.onOptionChange} sec_title2='File date' name2='file_date' value2={this.props.metadata.file_date} sec_title3='File size' name3='file_size' value3={this.props.metadata.file_size} sec_title4='File extension' name4='file_extension' value4={this.props.metadata.file_extension} />
 
 	
 
