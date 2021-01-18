ee4db67216d55ec0e108f7ebcb1507afaebdee22
Shuai Weng <sweng@stanford.edu>
2021-01-17 21:39:35 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import TwoColTextField from '../allele/twoColTextField';
-
-// import AutocompleteSection from '../phenotype/autocompleteSection';
+import AutocompleteSection from '../phenotype/autocompleteSection';
 
 class OneMetadata extends Component {
   constructor(props) {
@@ -15,10 +13,10 @@ class OneMetadata extends Component {
   render() {
 	
     return (
-
       <div>
         <div>SGDID: {this.props.metadata.sgdid} <a href={this.props.metadata.s3_url} target='new'>s3_url</a></div>
         <hr />	
+
         {/* file display name & previous file name & description */}
         <div className='row'>
           <div className='columns medium-4 small-4'>
@@ -35,7 +33,6 @@ class OneMetadata extends Component {
           </div>
         </div>
 
-	
         {/* file year, date, size, extension & file status */}
         <div className='row'>
           <div className='columns medium-1 small-1'>
@@ -62,11 +59,34 @@ class OneMetadata extends Component {
             <div> <label> keyword(s) ('|' delimited)</label> </div>
             <input type='text' name='keywords' value={this.props.metadata.keywords} onChange={this.props.onOptionChange} />
           </div>
-	
-
-
         </div>
 
+        {/* data_id, topic_id, & format_id */}
+        <div className='row'>
+          <div className='columns medium-4 small-4'>
+            <div> <label> data_id (EDAM Term) </label> </div>
+            <AutocompleteSection sec_title='' id='data_id' value1='display_name' value2='' selectedIdName='data_id' placeholder='Enter EDAM Term' onOptionChange={this.props.onOptionChange} selectedId={this.props.metadata.data_id} setNewValue={false} />
+          </div>
+          <div className='columns medium-4 small-4'>
+            <div> <label> topic_id (EDAM Term) </label> </div>
+            <AutocompleteSection sec_title='' id='topic_id' value1='display_name' value2='' selectedIdName='topic_id' placeholder='Enter EDAM Term' onOptionChange={this.props.onOptionChange} selectedId={this.props.metadata.topic_id} setNewValue={false} />
+          </div>
+          <div className='columns medium-4 small-4'>
+            <div> <label> format_id (EDAM Term) </label> </div>
+            <AutocompleteSection sec_title='' id='format_id' value1='display_name' value2='' selectedIdName='format_id' placeholder='Enter EDAM Term' onOptionChange={this.props.onOptionChange} selectedId={this.props.metadata.format_id} setNewValue={false} />
+          </div>  
+        </div>
+
+        {/* path_id & readme_file_id */}
+        <div className='row'>
+          <div className='columns medium-6 small-6'>
+            <div> <label> path_id (file path) </label> </div>
+            <AutocompleteSection sec_title='' id='path_id' value1='display_name' value2='' selectedIdName='path_id' placeholder='Enter file path' onOptionChange={this.props.onOptionChange} selectedId={this.props.metadata.path_id} setNewValue={false} />
+          </div>
+          <div className='columns medium-6 small-6'>
+            <div> <label> path_id (file path) </label> </div>
+            <AutocompleteSection sec_title='' id='readme_file_id' value1='display_name' value2='' selectedIdName='readme_file_id' placeholder='Enter README file' onOptionChange={this.props.onOptionChange} selectedId={this.props.metadata.readme_file_id} setNewValue={false} />
+          </div>
 
 	
       </div>
