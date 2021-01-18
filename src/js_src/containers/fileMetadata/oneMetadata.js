12d2536931842c7c1e9a4b7f196c02a597f81ed8
Shuai Weng <sweng@stanford.edu>
2021-01-17 20:50:41 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -17,8 +17,7 @@ class OneMetadata extends Component {
     return (
 
       <div>
-        <div>SGDID: {this.props.metadata.sgdid}</div>
-        <div>s3_url: {this.props.metadata.s3_url}</div>
+        <div>SGDID: {this.props.metadata.sgdid} <a href='{this.props.metadata.s3_url}' target='new'>s3_url</a></div>
 	
         {/* file display name & previous file name & description */}
         <div className='row'>
