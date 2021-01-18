be7cbab105c1548d1b2b34ffaf93621132e2e44a
Shuai Weng <sweng@stanford.edu>
2021-01-17 21:53:39 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -64,15 +64,15 @@ class OneMetadata extends Component {
         {/* is_in_browser etc */}
         <div className='row'>
           <div className='columns medium-4 small-4'>
-            <div> <label> is_in_browser </div>
+            <div> <label> is_in_browser </label></div>
             <input type='text' name='is_in_browser' value={this.props.metadata.is_in_browser} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-4 small-4'>
-            <div> <label> is_in_spell </div>
+            <div> <label> is_in_spell </label></div>
             <input type='text' name='is_in_spell' value={this.props.metadata.is_in_spell} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-4 small-4'>
-            <div> <label> is_public </div>
+            <div> <label> is_public </label></div>
             <input type='text' name='is_public' value={this.props.metadata.is_public} onChange={this.props.onOptionChange} />
           </div>	
         </div>
