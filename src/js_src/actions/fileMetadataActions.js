91b0410e987c7eb94eb5e0fe3cbb1feb1ac38682
Shuai Weng <sweng@stanford.edu>
2021-01-17 20:37:57 -0800

    testing

--- src/js_src/actions/fileMetadataActions.js
+++ src/js_src/actions/fileMetadataActions.js
@@ -1,4 +1,4 @@
 const SET_FILE_METADATA = 'SET_FILE_METADATA';
-export function setFileMetadata(currentFileMetadata) {
-  return { type: SET_FILE_METADATA, payload: { currentFileMetadata: currentFileMetadata } };
+export function setFileMetadata(currentMetadata) {
+  return { type: SET_FILE_METADATA, payload: { currentMetadata: currentMetadata } };
 }
