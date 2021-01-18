a9e20a62c0a838fea2b629107ea4656a7dabcd0a
Shuai Weng <sweng@stanford.edu>
2021-01-17 19:10:49 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import TwoColTextField from '../allele/twoColTextField';
+
 // import AutocompleteSection from '../phenotype/autocompleteSection';
 
 class OneMetadata extends Component {
@@ -21,16 +23,8 @@ class OneMetadata extends Component {
         {this.props.metadata.s3_url}
 	
         {/* file display name & previous file name*/}
-        <div className='row'>
-          <div className='columns medium-5 small-5'>
-            <div> <label> File display name </label> </div>
-            <input type='text' name='display_name' value={this.props.metadata.display_name} onChange={this.props.onOptionChange} />
-          </div>
-          <div className='columns medium-5 small-5'>
-            <div> <label> Previous file name </label> </div>
-            <input type='text' name='previous_file_name' value={this.props.metadata.previous_file_name} onChange={this.props.onOptionChange} />
-          </div>
-        </div>
+        <TwoColTextField sec_title='File display name' name='display_name' value={this.props.metadata.display_name} onOptionChange={this.props.onOptionChange} sec_title2='Previous file name' name2='previous_file_name' value2={this.props.metadata.previous_file_name} onOptionChange2={this.props.onOptionChange} />
+
 	
         {/* file year, date, size, extension & file status */}
         <div className='row'>
@@ -38,7 +32,7 @@ class OneMetadata extends Component {
             <div> <label> Year </label> </div>
             <input type='text' name='year' value={this.props.metadata.year} onChange={this.props.onOptionChange} />
           </div>
-          <div className='columns medium-2 small-2'>
+          <div className='columns medium-3 small-3'>
             <div> <label> File date </label> </div>
             <input type='text' name='file_date' value={this.props.metadata.file_date} onChange={this.props.onOptionChange} />
           </div>
@@ -50,7 +44,7 @@ class OneMetadata extends Component {
             <div> <label> File extension </label> </div>
             <input type='text' name='file_extension' value={this.props.metadata.file_extension} onChange={this.props.onOptionChange} />
           </div>
-          <div className='columns medium-2 small-2'>
+          <div className='columns medium-3 small-3'>
             <div> <label> File status </label> </div>
             <input type='text' name='dbentity_status' value={this.props.metadata.dbentity_status} onChange={this.props.onOptionChange} />
           </div>
