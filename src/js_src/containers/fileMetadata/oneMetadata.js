a9545c8143fdb1aff9142b54105e1e96baf56d3a
Shuai Weng <sweng@stanford.edu>
2021-01-17 19:04:55 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import TwoColTextField from '../allele/twoColTextField';
-
 // import AutocompleteSection from '../phenotype/autocompleteSection';
 
 class OneMetadata extends Component {
