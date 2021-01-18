67c80d8f13f0916d4c7273b845f93599cba1223d
Shuai Weng <sweng@stanford.edu>
2021-01-17 19:29:02 -0800

    testing

--- src/js_src/containers/fileMetadata/oneMetadata.js
+++ src/js_src/containers/fileMetadata/oneMetadata.js
@@ -25,15 +25,15 @@ class OneMetadata extends Component {
         {/* file display name & previous file name & description */}
         <div className='row'>
           <div className='columns medium-4 small-4'>
-            <div> <label> File display name </label> </div>
+            <div> <label> display_name </label> </div>
             <input type='text' name='display_name' value={this.props.metadata.display_name} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-4 small-4'>
-            <div> <label> Previous file name </label> </div>
+            <div> <label> previous_file_name </label> </div>
             <input type='text' name='previous_file_name' value={this.props.metadata.previous_file_name} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-4 small-4'>
-            <div> <label> Description </label> </div>
+            <div> <label> description </label> </div>
             <input type='text' name='descr' value={this.props.metadata.display_name} onChange={this.props.onOptionChange} />
           </div>
         </div>
@@ -42,27 +42,27 @@ class OneMetadata extends Component {
         {/* file year, date, size, extension & file status */}
         <div className='row'>
           <div className='columns medium-1 small-1'>
-            <div> <label> Year </label> </div>
+            <div> <label> year </label> </div>
             <input type='text' name='year' value={this.props.metadata.year} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-2 small-2'>
-            <div> <label> File_date </label> </div>
+            <div> <label> file_date </label> </div>
             <input type='text' name='file_date' value={this.props.metadata.file_date} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-2 small-2'>
-            <div> <label> File_size </label> </div>
+            <div> <label> file_size </label> </div>
             <input type='text' name='file_size' value={this.props.metadata.file_size} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-2 small-2'>
-            <div> <label> File_extension </label> </div>
+            <div> <label> file_extension </label> </div>
             <input type='text' name='file_extension' value={this.props.metadata.file_extension} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-2 small-2'>
-            <div> <label> File status </label> </div>
+            <div> <label> file_status </label> </div>
             <input type='text' name='dbentity_status' value={this.props.metadata.dbentity_status} onChange={this.props.onOptionChange} />
           </div>
           <div className='columns medium-3 small-3'>
-            <div> <label> Keywords </label> </div>
+            <div> <label> keyword(s) ('|' delimited)</label> </div>
             <input type='text' name='keywords' value={this.props.metadata.keywords} onChange={this.props.onOptionChange} />
           </div>
 	
