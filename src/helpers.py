a685cec77948dcac2a51b3d0dbdec2bfb0938ca2
Shuai Weng <sweng@stanford.edu>
2021-01-27 12:28:56 -0800

    testing

--- src/helpers.py
+++ src/helpers.py
@@ -332,8 +332,7 @@ def upload_file(username, file, **kwargs):
         DBSession.flush()
         fdb = DBSession.query(Filedbentity).filter(Filedbentity.dbentity_id == did).one_or_none()
         s3_url = fdb.upload_file_to_s3(file=file, filename=filename, is_web_file=is_web_file, file_path=full_file_path, flag=False)
-        if fdb.s3_url is None:
-            fdb.s3_url = s3_url
+        fdb.s3_url = s3_url
         DBSession.add(fdb)
         transaction.commit()
     except Exception as e:
