b68c26bf2f5c8bbd01b0763c8efdf21d6f94fbca
sweng66 <sweng@stanford.edu>
2021-02-02 12:53:27 -0800

    testing

--- scripts/loading/dataset/load_datasetsample.py
+++ scripts/loading/dataset/load_datasetsample.py
@@ -9,7 +9,7 @@ CREATED_BY = os.environ['DEFAULT_USER']
 
 log_file = "scripts/loading/dataset/logs/load_datasetsample.log"
 
-files_to_load = ["scripts/loading/dataset/data/datasample2020-01to07.tsv"]
+files_to_load = ["scripts/loading/dataset/data/datasample2020-08to2020-12.tsv"]
 
 DBXREF_URL = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc='
 
