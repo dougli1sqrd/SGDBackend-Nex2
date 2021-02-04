02b5bdce73a2fef484d937d1aa8cabf8de91bc1e
sweng66 <sweng@stanford.edu>
2021-02-04 15:41:11 -0800

    testing

---
+++ src/js_src/reducers/supplFileReducer.js
@@ -0,0 +1,18 @@
+const DEFAULT_STATE = {
+  currentSupplFile: {
+    files
+  }
+};
+
+const SET_SUPPL_FILE = 'SET_SUPPL_FILE';
+
+const supplFileReducer = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+  case SET_SUPPL_FILE:
+    return Object.assign({}, state, action.payload);
+  default:
+    return state;
+  }
+};
+
+export default supplFileReducer;
