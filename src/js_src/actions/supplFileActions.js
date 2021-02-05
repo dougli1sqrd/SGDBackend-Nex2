24a998053df99e645387fa3da609a41a7b6b336b
sweng66 <sweng@stanford.edu>
2021-02-05 11:44:14 -0800

    testing

--- src/js_src/actions/supplFileActions.js
+++ src/js_src/actions/supplFileActions.js
@@ -1,4 +1,4 @@
 const SET_SUPPL_FILE = 'SET_SUPPL_FILE';
-export function setSupplFile(currentAllele) {
+export function setSupplFile(currentSupplFile) {
   return { type: SET_SUPPL_FILE, payload: { currentSupplFile: currentSupplFile } };
 }
