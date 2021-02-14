7a29e79cae9b9cece1d0c1fff5c7521231421599
sweng66 <sweng@stanford.edu>
2021-02-14 10:34:29 -0800

    testing

---
+++ src/js_src/containers/dataset/editDataset.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import fetchData from '../../lib/fetchData';
+import Loader from '../../components/loader';
+import { connect } from 'react-redux';
+import { setError, setMessage } from '../../actions/metaActions';
+import { setDataset } from '../../actions/datasetActions';
+import { PREVIEW_URL } from '../../constants.js';
+import OneDataset from './oneDataset';
+const UPDATE_DATASET = '/dataset_update';
+const GET_DATASET = '/get_dataset_data';
+
+const TIMEOUT = 300000;
+
+class EditDataset extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  
+    this.state = {
+      dataset_id: null,
+      geo_id: null,
+      preview_url: null,	
+      isLoading: false,
+      isComplete: false,
+    };
+  }
+
+  componentDidMount() {
+    let url = this.setVariables();
+    this.getData(url);
+  }
+
+  handleChange() {
+    let currentDataset = {};
+    let data = new FormData(this.refs.form);
+    for (let key of data.entries()) {
+      currentDataset[key[0]] = key[1];
+    }
+    this.props.dispatch(setDataset(currentDataset));
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+    let formData = new FormData();
+    for(let key in this.props.dataset){
+      formData.append(key,this.props.dataset[key]);
+    }
+    fetchData(UPDATE_DATASET, {
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      timeout: TIMEOUT
+    }).then((data) => {
+      this.props.dispatch(setMessage(data.success));
+    }).catch((err) => {
+      this.props.dispatch(setError(err.error));
+    });
+  }
+
+  addButtons() {
+    return (
+      <div>
+        <div className='row'>
+          <div className='columns medium-6 small-6'>
+            <button type='submit' id='submit' value='0' className="button expanded" onClick={this.handleUpdate.bind(this)} > Update Dataset data </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  getData(url) {
+    this.setState({ isLoading: true });
+    fetchData(url).then( (data) => {
+      let currentDataset = {};
+      for (let key in data) {
+        currentDataset[key] = data[key];         
+      }
+      this.props.dispatch(setDataset(currentDataset));
+    })
+    .catch(err => this.props.dispatch(setError(err.error)))
+    .finally(() => this.setState({ isComplete: true, isLoading: false }));
+  }
+
+  setVariables() {
+    let urlList = window.location.href.split('/');
+    let geo_id = urlList[urlList.length-1];
+    let url = GET_DATASET + '/' + geo_id;  
+    this.setState({
+      geo_id: geo_id,
+      preview_url: `${PREVIEW_URL}` + '/dataset/' + geo_id
+    });
+    return url;
+  }
+    
+  displayForm() {
+    return (
+      <div>
+        <a href={this.state.preview_url} target='new'>Preview this Dataset Page</a>
+        <form onSubmit={this.handleUpdate} ref='form'>
+          <input name='GEOID' value={this.props.dataset.format_name} className="hide" />
+          <OneDataset dataset={this.props.dataset} onOptionChange={this.handleChange} />
+          {this.addButtons()}          	
+        </form>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <div>Please wait while we are constructing the update form.</div>
+          <div><Loader /></div>
+        </div>
+      );
+    }
+    if (this.state.isComplete) {
+      return this.displayForm();
+    }
+    else {
+      return (<div>Something is wrong while we are constructing the update form.</div>);
+    }
+  }
+}
+
+EditDataset.propTypes = {
+  dispatch: PropTypes.func,
+  dataset: PropTypes.object
+};
+
+
+function mapStateToProps(state) {
+  return {
+    dataset: state.dataset['currentDataset']
+  };
+}
+
+export default connect(mapStateToProps)(EditDataset);
