a8dd6a2b0eff7903e9e331f380427724c8c0d6c7
sweng66 <sweng@stanford.edu>
2021-02-15 12:11:51 -0800

    testing

--- src/dataset_helpers.py
+++ src/dataset_helpers.py
@@ -29,8 +29,49 @@ def insert_dataset_reference(curator_session, CREATED_BY, source_id, dataset_id,
 
 def get_list_of_dataset(request):
 
-    return []
+    try:
+        query = str(request.matchdict['query'])
+        data = []
 
+        ## search by PMID:
+        rows_by_pmid = []
+        if query.isdigit():
+            pmid = int(query)
+            ref = DBSession.query(Referencedbentity).filter_by(pmid=pmid).one_or_none()
+            if ref is not None:
+                all_dsRefs = DBSession.query(DatasetReference).filter_by(reference_id=ref.dbentity_id).all()
+                for x in all_dsRefs:
+                    rows_by_pmid.append(x.dataset)
+
+        ## search by GEO/SRA/ArrayExpress ID:
+        rows_by_GEO = DBSession.query(Dataset).filter(Dataset.format_name.ilike('%'+query+'%')).all()
+
+        ## search by file name:
+        rows_by_filename = []
+        all_fileRows = DBSession.query(Filedbentity).filter(or_(Filedbentity.display_name.ilike('%'+query+'%'), Filedbentity.previous_file_name.ilike('%'+query+'%'))).order_by(Filedbentity.display_name).all()
+        for x in all_fileRows:
+            all_dsFiles = DBSession.query(DatasetFile).filter_by(file_id=x.dbentity_id).all()
+            for y in all_dsFiles:
+                rows_by_filename.append(y.dataset)
+        
+        foundDataset = {}    
+        for x in rows_by_pmid + rows_by_GEO + rows_by_filename:
+            if x.format_name in foundDataset:
+                continue
+            foundDataset[x.format_name] = 1
+            data.append({ 'format_name': x.format_name,
+                          'display_name': x.display_name,
+                          'dbxref_id': x.dbxref_id,
+                          'dbxref_type': x.dbxref_type,
+                          'date_public': x.date_public })
+        return HTTPOk(body=json.dumps(data),content_type='text/json')
+    except Exception as e:
+        log.error(e)
+        return HTTPBadRequest(body=json.dumps({'error': str(e)}))
+    finally:
+        if DBSession:
+            DBSession.remove()
+            
 def get_one_dataset(request):
 
     return {}
