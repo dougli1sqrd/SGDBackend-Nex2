076bbd41b3172155da7dd2d0673653f421a90f02
sweng66 <sweng@stanford.edu>
2021-02-18 17:10:30 -0800

    testing

--- src/js_src/containers/dataset/navigateBar.js
+++ src/js_src/containers/dataset/navigateBar.js
@@ -4,24 +4,24 @@ import PropTypes from 'prop-types';
 class NavigateBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dataset_url: '/dataset/' + this.props.dataset_name,
-      sample_url: '/datasetsample/' + this.props.dataset_name,
-      track_url: '/datasettrack/' +	this.props.dataset_name
-    };
+    this.state = {};
   }
 
   render() {
+
+    let dataset_url = '/dataset/' + this.props.dataset_name;
+    let sample_url = '/datasetsample/' + this.props.dataset_name;
+    let track_url = '/datasettrack/' + this.props.dataset_name;
     return (
       <div className='row'>
         <div className='columns medium-4 small-4'>
-          <button type="button" className="button expanded" onClick={()=>window.open({this.state.dataset_url})} disabled={this.props.isDataset}>Update Dataset</button>
+          <button type="button" className="button expanded" onClick={()=>window.open({dataset_url})} disabled={this.props.isDataset}>Update Dataset</button>
         </div>
         <div className='columns medium-4 small-4'>
-          <button type="button" className="button expanded" onClick={()=>window.open({this.state.sample_url})} disabled={this.props.isSample}>Update Dataset Samples</button>          
+          <button type="button" className="button expanded" onClick={()=>window.open({sample_url})} disabled={this.props.isSample}>Update Dataset Samples</button>          
         </div>
         <div className='columns medium-4 small-4'>
-          <button type="button" className="button expanded" onClick={()=>window.open({this.state.track_url})} disabled={this.props.isTrack}>Update Dataset Tracks</button>
+          <button type="button" className="button expanded" onClick={()=>window.open({track_url})} disabled={this.props.isTrack}>Update Dataset Tracks</button>
         </div>
       </div>
     );
