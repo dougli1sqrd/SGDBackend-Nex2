ddf64eabbfc68de44e2a1500b9038b39f4a3365c
sweng66 <sweng@stanford.edu>
2021-02-20 15:50:03 -0800

    testing

--- src/js_src/containers/dataset/sampleSection.js
+++ src/js_src/containers/dataset/sampleSection.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import fetchData from '../../lib/fetchData';
-import { connect } from 'react-redux';
 import { setError, setMessage } from '../../actions/metaActions';
 
 const UPDATE_SAMPLE = '/datasetsample_update';
