f9f289373a437bbf0a94fb38f27dd6ddc59318fe
sweng66 <sweng@stanford.edu>
2021-02-20 14:14:03 -0800

    testing

--- src/js_src/containers/dataset/sampleSection.js
+++ src/js_src/containers/dataset/sampleSection.js
@@ -81,11 +81,11 @@ class SampleSection extends Component {
         <div className='row'>
           <div className='columns medium-4 small-4'>
             <div> <label> format_name </label> </div>
-            <input type='text' name='format_name' value={this.props.sample.format_name} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='format_name' value={this.props.data.format_name} onChange={this.handleChange.bind(this)} />
           </div>
           <div className='columns medium-8 small-8'>
             <div> <label> display_name </label> </div>
-            <input type='text' name='display_name' value={this.props.sample.display_name} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='display_name' value={this.props.data.display_name} onChange={this.handleChange.bind(this)} />
           </div>
         </div>
 
@@ -93,23 +93,23 @@ class SampleSection extends Component {
         <div className='row'>
           <div className='columns medium-2 small-2'>
             <div> <label> dbxref_id </label> </div>
-            <input type='text' name='dbxref_id' id={this.props.index} value={this.props.sample.dbxref_id} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='dbxref_id' value={this.props.data.dbxref_id} onChange={this.handleChange.bind(this)} />
           </div>
           <div className='columns medium-2 small-2'>
             <div> <label> dbxref_type </label> </div>
-            <input type='text' name='dbxref_type' value={this.props.sample.dbxref_type} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='dbxref_type' value={this.props.data.dbxref_type} onChange={this.handleChange.bind(this)} />
           </div>
           <div className='columns medium-3 small-3'>
             <div> <label> strain_name </label> </div>
-            <input type='text' name='strain_name' value={this.props.sample.strain_name} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='strain_name' value={this.props.data.strain_name} onChange={this.handleChange.bind(this)} />
           </div>	    
           <div className='columns medium-3 small-3'>
             <div> <label> biosample </label> </div>
-            <input type='text' name='biosample' value={this.props.sample.biosample} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='biosample' value={this.props.data.biosample} onChange={this.handleChange.bind(this)} />
           </div>
           <div className='columns medium-2 small-2'>
             <div> <label> sample_order </label> </div>
-            <input type='text' name='sample_order' value={this.props.sample.sample_order} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='sample_order' value={this.props.data.sample_order} onChange={this.handleChange.bind(this)} />
           </div>
         </div>
 
@@ -117,7 +117,7 @@ class SampleSection extends Component {
         <div className='row'>
           <div className='columns medium-12 small-12'>
             <div> <label> dbxref_url </label> </div>
-            <input type='text' name='dbxref_url' value={this.props.sample.dbxref_url} onChange={this.handleChange.bind(this)} />
+            <input type='text' name='dbxref_url' value={this.props.data.dbxref_url} onChange={this.handleChange.bind(this)} />
           </div>
         </div>
 
@@ -143,11 +143,11 @@ class SampleSection extends Component {
   }
     
   render() {
-    let formName = 'form' + this.props.index;
+    //let formName = 'form' + this.props.index;
     return (
       <div>
-        <form onSubmit={this.handleUpdate} ref={formName}>
-          <input name='datasetsample_id' value={this.props.sample.datasetsample_id} className="hide" />
+        <form onSubmit={this.handleUpdate} ref='form'>
+          <input name='datasetsample_id' value={this.props.data.datasetsample_id} className="hide" />
           {this.sampleRow()}
           <hr />
         </form>
