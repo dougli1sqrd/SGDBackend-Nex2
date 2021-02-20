113eb1000162cc34f767f3267004f94dd22cfbb0
sweng66 <sweng@stanford.edu>
2021-02-20 08:46:11 -0800

    testing

--- src/js_src/containers/dataset/editSample.js
+++ src/js_src/containers/dataset/editSample.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CurateLayout from '../curateHome/layout';
-import { setSample } from '../../actions/datasetSampleActions';  
+// import { setSample } from '../../actions/datasetSampleActions';  
 import fetchData from '../../lib/fetchData';
 import SampleSection from './sampleSection';
 import { setError, setMessage } from '../../actions/metaActions';
