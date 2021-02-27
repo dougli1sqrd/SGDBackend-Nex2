8812f0aa322a798eb47cda1807b322afc5299adc
sweng66 <sweng@stanford.edu>
2021-02-26 22:11:59 -0800

    testing

--- src/dataset_helpers.py
+++ src/dataset_helpers.py
@@ -260,7 +260,7 @@ def load_dataset(request):
             file = fileObj.file
             filename = fileObj.filename
 
-        return HTTPBadRequest(body=json.dumps({'error': "file="+str(file)}), content_type='text/json')
+        # return HTTPBadRequest(body=json.dumps({'error': "file="+str(file)}), content_type='text/json')
 
             
         if file is None or filename is None:
