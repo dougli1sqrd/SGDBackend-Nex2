6bb9770eb4d1d69a431670f085e9d9f7161509f8
sweng66 <sweng@stanford.edu>
2021-04-02 14:37:22 -0700

    testing

--- scripts/dumping/sequence_update/dump_restmap_dataset.py
+++ scripts/dumping/sequence_update/dump_restmap_dataset.py
@@ -1,50 +1,31 @@
-from scripts.loading.database_session import get_session
-from src.models import Locusdbentity, Dnasequenceannotation, Taxonomy, Contig
 import sys
+from scripts.loading.database_session import get_session
+from src.models import Locusdbentity, Dnasequenceannotation, Taxonomy, Contig, So,\
+                       Proteinsequenceannotation
+from scripts.dumping.sequence_update import generate_dna_seq_file
 
 __author__ = 'sweng66'
 
-datafile = "scripts/dumping/sequence_update/data/restrictionMapper/orf_genomic.seq"
+genomicFile = "scripts/dumping/sequence_update/data/restrictionMapper/orf_genomic.seq"
 
 TAXON = "TAX:559292"
+FILE_TYPE = 'restmapper'
 
 def dump_data():
 
     nex_session = get_session()
 
-    fw = open(datafile, "w")
-    
     taxonomy = nex_session.query(Taxonomy).filter_by(taxid=TAXON).one_or_none()
     taxonomy_id = taxonomy.taxonomy_id
 
-    dbentity_id_to_data = dict([(x.dbentity_id, (x.systematic_name, x.gene_name, x.sgdid, x.qualifier, x.description)) for x in nex_session.query(Locusdbentity).all()])
+    dbentity_id_to_data = dict([(x.dbentity_id, (x.systematic_name, x.gene_name, x.sgdid, x.qualifier, x.description)) for x in nex_session.query(Locusdbentity).filter_by(dbentity_status = 'Active').all()])
 
-    contig_id_to_chr = dict([(x.contig_id, x.display_name) for x in nex_session.query(Contig).filter(Contig.display_name.like('Chromosome %')).all()])
-
-    for x in nex_session.query(Dnasequenceannotation).filter_by(taxonomy_id=taxonomy_id, dna_type='GENOMIC').all():
-        if x.contig_id not in contig_id_to_chr:
-            continue
-        if x.dbentity_id not in dbentity_id_to_data:
-            continue
-        (systematic_name, gene_name, sgdid, qualifier, desc) = dbentity_id_to_data[x.dbentity_id]
-        chr = contig_id_to_chr[x.contig_id].replace('Chromosome ', 'Chr ')
-        if gene_name is None:
-            gene_name = ''
-        start_index = x.start_index
-        end_index = x.end_index
-        if x.strand == '-':
-            (start_index, end_index) = (end_index, start_index)
-        defline = ">" + systematic_name + " " + gene_name + " SGDID:" + sgdid + ", " + chr + " from " + str(start_index) + "-" + str(end_index) + ", Genome Release 64-3-1, "
-        if x.strand == '-':
-            defline = defline + "reverse complement, "
-        if qualifier is not None:
-            defline = defline + qualifier + " ORF, "
-        if desc is not None:
-            defline = defline + '"' + desc + '"'
-        fw.write(defline + "\n")
-        fw.write(x.residues + "\n")
+    so_id_to_display_name = dict([(x.so_id, x.display_name) for x in nex_session.query(So).all()])
     
-    fw.close()
+    contig_id_to_chr = dict([(x.contig_id, x.display_name) for x in nex_session.query(Contig).filter(Contig.display_name.like('Chromosome %')).all()])
+        
+    generate_dna_seq_file(nex_session, taxonomy_id, dbentity_id_to_data, contig_id_to_chr,
+                          so_id_to_display_name, genomicFile, 'GENOMIC', FILE_TYPE)
 
     nex_session.close()
 
