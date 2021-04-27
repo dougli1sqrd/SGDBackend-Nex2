926d90e07c7a7e4e9766d2b4165f2ee24fe1146c
sweng66 <sweng@stanford.edu>
2021-04-26 20:11:01 -0700

    fixing bugs

--- src/js_src/containers/dataset/oneSample.js
+++ src/js_src/containers/dataset/oneSample.js
@@ -127,7 +127,7 @@ class OneSample extends Component {
         <div className='row'>
           <div className='columns medium-12 small-12'>
             <div> <label> assay_id (OBI Term) </label> </div>
-            <AutocompleteSection sec_title='' id='assay_id' value1='display_name' value2='' selectedIdName='assay_id' placeholder='Enter OBI Term' onOptionChange={this.props.onOptionChange} selectedId={this.props.dataset.assay_id} setNewValue={false} />
+            <AutocompleteSection sec_title='' id='assay_id' value1='display_name' value2='' selectedIdName='assay_id' placeholder='Enter OBI Term' onOptionChange={this.props.onOptionChange} selectedId={this.state.data.assay_id} setNewValue={false} />
           </div>
         </div>
 	
